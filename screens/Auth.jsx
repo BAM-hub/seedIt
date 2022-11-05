87b2f9103b520fcb350dfa95649218f7b365df55
BAM-hub <bsharamin12@gmail.com>
2022-11-05 02:53:34 +0200

    added functionality

--- screens/Auth.jsx
+++ screens/Auth.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Login from '../components/Login';
 import Register from '../components/Register';
 import {
@@ -8,21 +8,49 @@ import {
   TextInput,
   Text,
 } from '@react-native-material/core';
-import { Dimensions, StyleSheet, ImageBackground } from 'react-native';
+import {
+  Dimensions,
+  StyleSheet,
+  ImageBackground,
+  LayoutAnimation,
+  NativeModules,
+} from 'react-native';
 import background from '../assets/background.jpg';
 
-import { COLOR_PRIMARY } from '../constants';
+const { UIManager } = NativeModules;
+
+UIManager.setLayoutAnimationEnabledExperimental &&
+  UIManager.setLayoutAnimationEnabledExperimental(true);
+
+import { COLOR_PRIMARY } from '../config';
 
 const Auth = () => {
-  const [active, setActive] = useState(null);
+  const [margin, setMargin] = useState('300%');
+  const [active, setActive] = useState();
+
+  const toggleActive = target => {
+    setActive(target);
+    LayoutAnimation.configureNext({
+      duration: 500,
+      create: { type: 'linear', property: 'opacity' },
+      update: { type: 'spring', springDamping: 0.4 },
+      delete: { type: 'linear', property: 'opacity' },
+    });
+    setMargin('40%');
+  };
+
   return (
     <Flex center style={{ height: Dimensions.get('window').height }}>
       <ImageBackground source={background} style={styles.image}>
-        {active !== null ? (
+        {active ? (
           active === 'login' ? (
-            <Login />
+            <Flex center style={{ ...styles.container, marginTop: margin }}>
+              <Login toggleActive={toggleActive} />
+            </Flex>
           ) : (
-            <Register />
+            <Flex center style={{ ...styles.container, marginTop: margin }}>
+              <Register toggleActive={toggleActive} />
+            </Flex>
           )
         ) : (
           <Flex center style={styles.buttonContainer}>
@@ -48,6 +76,7 @@ const Auth = () => {
                   fontSize: 16,
                 }}
                 title="Register"
+                onPress={() => toggleActive('Register')}
               />
             </Box>
           </Flex>
@@ -58,6 +87,16 @@ const Auth = () => {
 };
 
 const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '70%',
+    // marginTop: '40%',
+    backgroundColor: '#CFFFB3',
+    borderRadius: 50,
+    borderBottomRightRadius: 0,
+    borderBottomLeftRadius: 0,
+    padding: 20,
+  },
   buttonContainer: {
     height: Dimensions.get('window').height / 4,
     justifyContent: 'space-evenly',
