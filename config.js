c06ff33f635ce690afd3cb13db51c5c73c262e7b
BAM-hub <bsharamin12@gmail.com>
2022-11-20 01:07:49 +0300

    mock server url

--- config.js
+++ config.js
@@ -3,4 +3,4 @@ export const COLOR_SECONDARY = '#3B7080';
 export const COLOR_COMPLEMENTARY = '#3B7080';
 export const COLOR_COMPLEMENTARY_SECONDARY = '#CFFFB3';
 export const COLOR_ACCENT = '#FCEC52';
-export const MOCK_SERVER_URL = 'http://10.0.2.2:3000';
+export const MOCK_SERVER_URL = 'https://10.0.2.2:44318/api';
