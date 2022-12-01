2edf7e7ffde2916561b0282cc7fae53393ab4186
BAM-hub <bsharamin12@gmail.com>
2022-12-02 01:50:18 +0300

    new styles for the explore tab design

--- screens/Explore.jsx
+++ screens/Explore.jsx
@@ -67,6 +67,38 @@ const Explore = () => {
     },
   ]);
 
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      image:
+        'https://i.pinimg.com/736x/47/8e/d1/478ed1fb9342c571505643a0e622c4ec.jpg',
+      title: 'Getting Started',
+      userName: 'John Doe',
+      userImage: 'https://i.pravatar.cc/150?img=1',
+      tags: ['Nature', 'Learning', 'Getting Started'],
+      liked: false,
+    },
+    {
+      id: 2,
+      image:
+        'https://i.pinimg.com/736x/47/8e/d1/478ed1fb9342c571505643a0e622c4ec.jpg',
+      title: 'Getting Started',
+      userName: 'John Doe',
+      userImage: 'https://i.pravatar.cc/150?img=1',
+      tags: ['Nature', 'Learning', 'Getting Started'],
+      liked: true,
+    },
+    {
+      id: 3,
+      image: '',
+      title: 'Getting Started',
+      userName: 'John Doe',
+      userImage: '',
+      tags: ['Nature', 'Learning', 'Getting Started'],
+      liked: true,
+    },
+  ]);
+
   const renderItem = ({ item }) => (
     <View
       style={{
@@ -85,47 +117,45 @@ const Explore = () => {
 
   return (
     <View>
-      <ScrollView>
-        <View
-          style={{
+      <View
+        style={{
+          width: '100%',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          padding: 20,
+        }}>
+        <Input
+          containerStyle={{
+            width: '80%',
+            backgroundColor: 'white',
+            height: 50,
+            borderRadius: 10,
+          }}
+          inputContainerStyle={{
             width: '100%',
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            // alignItems: 'center',
-            padding: 20,
-          }}>
-          <Input
-            containerStyle={{
-              width: '80%',
-              backgroundColor: 'white',
+            backgroundColor: 'white',
+            borderBottomWidth: 0,
+            height: '100%',
+          }}
+          leftIcon={() => <Icon name="md-search" size={30} color="black" />}
+        />
+
+        <TouchableOpacity onPress={() => setShowModal(true)}>
+          <View
+            style={{
+              width: 50,
               height: 50,
-              borderRadius: 10,
-            }}
-            inputContainerStyle={{
-              width: '100%',
-              backgroundColor: 'white',
-              borderBottomWidth: 0,
-              height: '100%',
-            }}
-            leftIcon={() => <Icon name="md-search" size={30} color="black" />}
-          />
-
-          <TouchableOpacity onPress={() => setShowModal(true)}>
-            <View
-              style={{
-                width: 50,
-                height: 50,
-                backgroundColor: '#cccccc',
-                borderRadius: 50,
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}>
-              <AntdIcon name="user" size={30} color="black" />
-              {/* <Image */}
-            </View>
-          </TouchableOpacity>
-        </View>
-
+              backgroundColor: '#cccccc',
+              borderRadius: 50,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <AntdIcon name="user" size={30} color="black" />
+            {/* <Image */}
+          </View>
+        </TouchableOpacity>
+      </View>
+      <ScrollView style={{ marginBottom: 90 }}>
         <FlatList
           data={stories}
           renderItem={renderItem}
@@ -190,83 +220,142 @@ const Explore = () => {
             ))}
           </ScrollView>
         </View>
-        <View
+        <Text
           style={{
-            marginTop: 20,
-            marginBottom: 20,
-            alignItems: 'center',
-            justifyContent: 'center',
-            position: 'relative',
-            backgroundColor: 'white',
-            width: '90%',
-            // alignSelf: 'flex-end',
+            fontSize: 25,
+            fontWeight: 'bold',
+            margin: 20,
+            color: theme.colors.primary,
             alignSelf: 'center',
-            // marginRight: -20,
-            borderRadius: 10,
-            padding: 15,
-            flexDirection: 'row',
-            height: 250,
           }}>
-          <View style={{ height: '100%', justifyContent: 'space-around' }}>
-            <TouchableOpacity>
-              <AntdIcon name="hearto" size={30} color={theme.colors.primary} />
-            </TouchableOpacity>
-            <Text
+          Community
+        </Text>
+        {posts.map(post => (
+          <View
+            key={post.id}
+            style={{
+              marginTop: 20,
+              marginBottom: 20,
+              alignItems: 'center',
+              justifyContent: 'center',
+              position: 'relative',
+              backgroundColor: 'white',
+              width: '90%',
+              // alignSelf: 'flex-end',
+              alignSelf: 'center',
+              // marginRight: -20,
+              borderRadius: 10,
+              padding: 15,
+              flexDirection: 'row',
+              height: 250,
+            }}>
+            <View
               style={{
-                fontSize: 25,
-                fontWeight: 'bold',
-                color: theme.colors.primary,
+                height: '100%',
+                justifyContent: 'space-around',
+                width: '40%',
               }}>
-              First Post
-            </Text>
-            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-              <View
-                style={{
-                  width: 40,
-                  height: 40,
-                  backgroundColor: '#cccccc',
-                  borderRadius: 50,
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                }}>
-                <AntdIcon name="user" size={30} color="black" />
-                {/* <Image */}
-              </View>
+              {post.liked ? (
+                <TouchableOpacity>
+                  <AntdIcon
+                    name="heart"
+                    size={30}
+                    color={theme.colors.primary}
+                  />
+                </TouchableOpacity>
+              ) : (
+                <TouchableOpacity>
+                  <AntdIcon
+                    name="hearto"
+                    size={30}
+                    color={theme.colors.primary}
+                  />
+                </TouchableOpacity>
+              )}
+
               <Text
                 style={{
                   fontSize: 15,
                   fontWeight: 'bold',
                   color: theme.colors.primary,
-                  marginLeft: 5,
                 }}>
-                John Doe
+                {post.title}
               </Text>
+              <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                <View
+                  style={{
+                    width: 40,
+                    height: 40,
+                    backgroundColor: '#cccccc',
+                    borderRadius: 50,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}>
+                  {post.userImage ? (
+                    <Image
+                      source={{ uri: post.userImage }}
+                      style={{ width: 40, height: 40, borderRadius: 50 }}
+                    />
+                  ) : (
+                    <AntdIcon name="user" size={30} color="black" />
+                  )}
+                </View>
+                <Text
+                  style={{
+                    fontSize: 10,
+                    fontWeight: 'bold',
+                    color: theme.colors.primary,
+                    marginLeft: 5,
+                  }}>
+                  {post.userName}
+                </Text>
+              </View>
+            </View>
+            <View
+              style={{
+                width: 200,
+                height: 200,
+                // backgroundColor: 'red',
+                alignItems: 'flex-end',
+                // marginRight: -20,
+              }}>
+              {post.image ? (
+                <Image
+                  source={{
+                    uri: `${post.image}`,
+                  }}
+                  containerStyle={{
+                    width: 200,
+                    height: 200,
+                  }}
+                  style={{
+                    width: '100%',
+                    height: '100%',
+                  }}
+                  PlaceholderContent={<Text>Loading...</Text>}
+                />
+              ) : (
+                <View
+                  style={{
+                    width: 200,
+                    height: 200,
+                    backgroundColor: 'grey',
+                    justifyContent: 'center',
+                  }}>
+                  <Text
+                    style={{
+                      fontSize: 30,
+                      fontWeight: 'bold',
+                      color: theme.colors.primary,
+                      alignSelf: 'center',
+                    }}>
+                    No IMAGE
+                  </Text>
+                </View>
+              )}
             </View>
           </View>
-          <View
-            style={{
-              width: '70%',
-              height: 200,
-              // backgroundColor: 'red',
-              alignItems: 'flex-end',
-              // marginRight: -20,
-            }}>
-            <Image
-              source={{
-                uri: 'https://i.pinimg.com/564x/6f/e0/bf/6fe0bf5a6aeaaaf27f6ea247df228dee.jpg',
-              }}
-              containerStyle={{
-                width: '70%',
-                height: 200,
-              }}
-              style={{
-                width: '100%',
-                height: '100%',
-              }}
-              PlaceholderContent={<Text>Loading...</Text>}
-            />
-          </View>
-        </View>
+        ))}
       </ScrollView>
       <ProfileModal showModal={showModal} setShowModal={setShowModal} />
     </View>
@@ -281,12 +370,14 @@ const ProfileModal = ({ showModal, setShowModal }) => (
         onDismiss={() => setShowModal(false)}
         contentContainerStyle={{
           width: '90%',
-          height: '100%',
+          // height: '100%',
           backgroundColor: 'white',
           borderRadius: 20,
           padding: 20,
-          marginTop: 70,
+          // marginTop: 70,
           marginHorizontal: 20,
+          position: 'absolute',
+          top: 50,
         }}>
         <View
           style={{
@@ -321,7 +412,7 @@ const ProfileModal = ({ showModal, setShowModal }) => (
               }}>
               BAM 99
             </Text>
-            <Text>bsharamin12@gmail.com</Text>
+            <Text style={{ color: 'grey' }}>bsharamin12@gmail.com</Text>
           </View>
         </View>
         <View
