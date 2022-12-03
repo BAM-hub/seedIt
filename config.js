504c5268623fac074b6fca1d836cf5dfea458165
BAM-hub <bsharamin12@gmail.com>
2022-12-03 14:54:47 +0300

    not used anymore

--- config.js
+++
@@ -1,6 +0,0 @@
-export const COLOR_PRIMARY = '#3A5743';
-export const COLOR_SECONDARY = '#3B7080';
-export const COLOR_COMPLEMENTARY = '#3B7080';
-export const COLOR_COMPLEMENTARY_SECONDARY = '#CFFFB3';
-export const COLOR_ACCENT = '#FCEC52';
-export const MOCK_SERVER_URL = 'https://10.0.2.2:44318/api';
