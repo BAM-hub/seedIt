18ce3a35271ea11979a48b04f9012737dd9f8bc9
BAM-hub <bsharamin12@gmail.com>
2022-12-06 20:20:36 +0300

    exported client

---
+++ api/client.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const client = axios.create({
+  baseURL: 'http://10.0.2.2:8000/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+client.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  err => {
+    const { status } = err.response;
+    console.log({ status });
+    if (status === 401) {
+      throw new Error('Unauthorized');
+    }
+    if (status === 500) {
+      throw new Error('Server Error');
+    }
+  },
+);
+
+export default client;
