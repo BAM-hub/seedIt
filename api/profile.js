a22d051fe643cf8fee943d26c326d1ff88346fed
BAM-hub <bsharamin12@gmail.com>
2022-12-25 03:41:30 +0300

    update profile is adde now working on upload image

--- api/profile.js
+++ api/profile.js
@@ -1,24 +1,42 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import client from './client';
 
-export const createProfile = async profile => {
-  const token = await AsyncStorage.getItem('@token');
+export const createProfile = async ({ profile, token, userId }) => {
   const res = await client.post('/profile/CreateProfile', profile, {
     headers: {
       'x-auth-token': token,
+      'x-auth-id': userId,
     },
   });
   return res;
 };
 
-export const uploadProfileImage = async (image, id) => {
+export const updateProfile = async ({ profile, token, userId }) => {
+  console.log({ profile, token, userId });
+  const res = await client.put(
+    `/profile/UpdateProfile/${profile.id}`,
+    profile,
+    {
+      headers: {
+        'x-auth-token': token,
+        'x-auth-id': userId,
+      },
+    },
+  );
+  return res;
+};
+
+export const uploadProfileImage = async ({ image, id }) => {
   const formData = new FormData();
-  formData.append('image', image);
+  formData.append('image', image, image.name || 'image.jpg');
+  console.log({ image, id });
   const token = await AsyncStorage.getItem('@token');
+  console.log({ formData });
   const res = await client.post(`/profile/UploadProfileImage/${id}`, formData, {
     headers: {
+      'content-type': 'multipart/encrypted',
+
       'x-auth-token': token,
-      'Content-Type': 'multipart/form-data',
     },
   });
   return res;
