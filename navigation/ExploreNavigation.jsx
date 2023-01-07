acaa4277712b0f64ab4873f92ddae106bf028c94
BAM-hub <bsharamin12@gmail.com>
2023-01-07 22:41:15 +0300

    created a new  shared element stack navigation

---
+++ navigation/ExploreNavigation.jsx
@@ -0,0 +1,42 @@
+import { NavigationContainer } from '@react-navigation/native';
+import Explore from '../screens/Explore';
+import PlantCategoryScreen from '../screens/PlantCategoryScreen';
+import { createSharedElementStackNavigator } from 'react-navigation-shared-element';
+
+const Stack = createSharedElementStackNavigator();
+
+const ProfileNavigation = () => {
+  return (
+    <NavigationContainer independent={true}>
+      <Stack.Navigator
+        screenOptions={
+          {
+            // headerShown: false,
+          }
+        }
+        initialRouteName="explore">
+        <Stack.Screen name="explore" component={Explore} />
+        <Stack.Screen
+          name="plantCategory"
+          component={PlantCategoryScreen}
+          sharedElements={(route, otherRoute, showing) => {
+            const { category } = route.params;
+            return [
+              {
+                id: `item.${category.name}.photo`,
+              },
+              {
+                id: `item.${category.name}.text`,
+              },
+              {
+                id: 'general.bg',
+              },
+            ];
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default ProfileNavigation;
