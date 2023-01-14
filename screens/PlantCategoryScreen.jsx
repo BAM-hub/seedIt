5c7fc71b72574d91e31b47d1e669177034416804
BAM-hub <bsharamin12@gmail.com>
2023-01-14 04:06:19 +0300

    fix text croped

--- screens/PlantCategoryScreen.jsx
+++ screens/PlantCategoryScreen.jsx
@@ -5,15 +5,32 @@ import {
   Animated,
   TouchableOpacity,
   Dimensions,
+  ScrollView,
 } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import { SharedElement } from 'react-navigation-shared-element';
 import { useTheme } from '@rneui/themed';
 import MCI from 'react-native-vector-icons/MaterialCommunityIcons';
+import { useRef, useState, useEffect } from 'react';
+import { FlingGesture } from 'react-native-gesture-handler/lib/typescript/handlers/gestures/flingGesture';
+import {
+  Directions,
+  FlingGestureHandler,
+  State,
+} from 'react-native-gesture-handler';
+import { Image } from '@rneui/themed';
 
 const PlantCategoryScreen = ({ route, navigation }) => {
-  const { category } = route.params;
+  const { category, plantData } = route.params;
+  const [top, setTop] = useState(300 - 20);
+  const yPostion = useRef(new Animated.Value(300 - 20)).current;
   const { theme } = useTheme();
+  useEffect(() => {
+    Animated.spring(yPostion, {
+      toValue: top,
+      useNativeDriver: true,
+    }).start();
+  });
   return (
     <View
       style={{
@@ -97,17 +114,58 @@ const PlantCategoryScreen = ({ route, navigation }) => {
           </View>
         </Animated.View>
       </SharedElement>
-      <SharedElement id="general.bg">
-        <View
+      <SharedElement
+        id="general.bg"
+        style={{
+          width: Dimensions.get('screen').width,
+          height: Dimensions.get('screen').height - 60,
+          backgroundColor: 'transparent',
+          borderRadius: 20,
+        }}>
+        <Animated.View
           style={{
             position: 'absolute',
             width: Dimensions.get('screen').width,
-            height: Dimensions.get('screen').height,
+            height: Dimensions.get('screen').height - 60,
             backgroundColor: 'white',
-            zIndex: 100,
-            transform: [{ translateY: 300 - 20 }],
+            transform: [{ translateY: yPostion }],
             borderRadius: 20,
           }}>
+          <FlingGestureHandler
+            direction={Directions.UP}
+            onHandlerStateChange={ev => {
+              if (ev.nativeEvent.state === State.END) {
+                setTop(0);
+              }
+            }}>
+            <FlingGestureHandler
+              direction={Directions.DOWN}
+              onHandlerStateChange={ev => {
+                if (ev.nativeEvent.state === State.END) {
+                  setTop(300 - 20);
+                }
+              }}>
+              <View
+                style={{
+                  height: 20,
+                  width: '90%',
+                  alignSelf: 'center',
+                  justifyContent: 'center',
+                  borderBottomWidth: 0.5,
+                  borderBottomColor: '#d3d3d3',
+                }}>
+                <View
+                  style={{
+                    width: 40,
+                    height: 5,
+                    backgroundColor: '#d3d3d3',
+                    borderRadius: 5,
+                    alignSelf: 'center',
+                  }}
+                />
+              </View>
+            </FlingGestureHandler>
+          </FlingGestureHandler>
           <Text
             style={{
               fontSize: 20,
@@ -118,23 +176,46 @@ const PlantCategoryScreen = ({ route, navigation }) => {
             }}>
             Family Summary
           </Text>
-          <View
-            animation="fadeInUp"
-            delay={2000}
+          <ScrollView
             style={{
-              backgroundColor: theme.colors.primary,
-              width: 60,
-              height: 60,
-              borderRadius: 30,
-              alignItems: 'center',
-              justifyContent: 'center',
+              height: 20,
+              marginBottom: 20,
+              width: '90%',
+              alignSelf: 'center',
             }}>
-            <MCI name="flower" size={30} color={theme.colors.white} />
-          </View>
-        </View>
+            <Text
+              style={{
+                fontSize: 16,
+                color: theme.colors.secondary,
+                alignSelf: 'center',
+                letterSpacing: 1,
+                lineHeight: 20,
+                overflow: 'hidden',
+              }}>
+              {plantData[0].trim()}
+            </Text>
+          </ScrollView>
+          <View style={{ height: 59 }} />
+        </Animated.View>
       </SharedElement>
     </View>
   );
 };
+
+const mightUse = () => (
+  <View
+    animation="fadeInUp"
+    delay={2000}
+    style={{
+      backgroundColor: theme.colors.primary,
+      width: 60,
+      height: 60,
+      borderRadius: 30,
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}>
+    <MCI name="flower" size={30} color={theme.colors.white} />\
+  </View>
+);
 
 export default PlantCategoryScreen;
