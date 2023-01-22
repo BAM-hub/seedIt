b7f06a0666e04435a1e0419ec5079469457a309d
BAM-hub <bsharamin12@gmail.com>
2023-01-22 20:56:27 +0300

    get auth set user state

--- hooks/useGetAuthToken.js
+++ hooks/useGetAuthToken.js
@@ -3,11 +3,15 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { loginWithToken } from '../api/user';
 import useProfileStore from '../store/profileStore';
 import useUserStore from '../store/userStore';
+import client from '../api/client';
+import usePlantsStore from '../store/plantsStore';
 
 export default useGetAuthToken = () => {
-  const { setProfile } = useProfileStore();
+  const { setProfile, profile } = useProfileStore();
   const { setUser, resetUser } = useUserStore();
+  const { setUserPlants } = usePlantsStore();
   const queryClient = useQueryClient();
+
   const { data, isLoading, isError } = useQuery({
     queryKey: ['user'],
     queryFn: async () => {
@@ -28,8 +32,13 @@ export default useGetAuthToken = () => {
         await AsyncStorage.setItem('@token', token);
         queryClient.setQueryData(['user'], { token, user });
         setUser({ token, ...user });
-
         setProfile(user.profile);
+        const res = await client.get(
+          `/Account/getProfileById/${user.profile.id}`,
+        );
+        console.log(res.data, 'res');
+        // console.log('res', `/Account/getProfileById/${user.profile.id}`, res);
+        setUserPlants(res.data.plant);
       } catch (error) {
         console.log(error);
       }
